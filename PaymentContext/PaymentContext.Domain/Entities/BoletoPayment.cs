006e3549679aa0f3127b399cef92e360f6c9aac4
isabela <bel.alves2012@gmail.com>
2020-05-06 23:49:57 -0300

    (S) Aplicando responsabilidade unica no dominio e (O) principio aberto e fechado nas entidades.

---
+++ PaymentContext/PaymentContext.Domain/Entities/BoletoPayment.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentContext.Domain.Entities
+{
+    public class BoletoPayment : Payment
+    {
+        public BoletoPayment(
+            string barCode,
+            string boletoNumber,
+            DateTime paidDate,
+            DateTime expireDate,
+            decimal total,
+            decimal totalPaid,
+            string owner,
+            string document,
+            string address,
+            string email) : base(
+                paidDate,
+                expireDate,
+                total,
+                totalPaid,
+                owner,
+                document,
+                address,
+                email)
+        {
+            BarCode = barCode;
+            BoletoNumber = boletoNumber;
+        }
+
+        public string BarCode { get; private set; }
+        public string BoletoNumber { get; private set; }
+
+    }
+}
---
+++ PaymentContext/PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentContext.Tests.Entities
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void Teste()
+        {
+            var student = new Student("Isabela","Santos","123244222","bel.alves2012@gmail.com");
+            //var subscription = new Subscription(DateTime.Now.Add()); 
+            //student.Subscriptions.ad
+
+            /**SOLID
+            S= Single responsabilidade unica (cada um no sei quadrado)
+                -é o que estamos fazendo com as classes, cada uma com sua devida responsabilidade
+
+            O = Open-Closed Principle (Princípio Aberto-Fechado)
+                - é o que estamos fazendo com nossas propriedades colocando a propriedade de escrita 
+                  (set) como privada, para deixar qualquer alteração ser feita somente atraves de metodos
+                - nossa classe tbm está aberta para ser utilizada e fechada para ser modificada.
+             
+             
+            Livro - Cleab Code e Clean Coder (sobre SOLID)
+             */
+        }
+
+    }
+}
