3387081b3bd9d3355d697b476535010500c9a7a7
Isabela Alves <bel.alves2012@gmail.com>
2020-05-23 12:44:29 -0300

    Criando testes para o VO Document.

---
+++ PaymentContext/PaymentContext.Tests/ValueObjects/DocumentTestes.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentContext.Tests.ValueObjects
+{
+    [TestClass]
+    public class DocumentTestes
+    {
+        //Red, Green, Refactor
+
+        [TestMethod]
+        public void ShouldReturnErrorWhenCNPJIsInvalid()
+        {
+            var doc = new Document("123", EDocumentType.CNPJ);
+            Assert.IsTrue(doc.Invalid);
+        }
+        [TestMethod]
+        public void ShouldReturnSuccessWhenCNPJIsValid()
+        {
+            var doc = new Document("24736744000137", EDocumentType.CNPJ);
+            Assert.IsTrue(doc.Valid);
+        }
+
+        [TestMethod]
+        public void ShouldReturnErrorWhenCPFIsInvalid()
+        {
+            var doc = new Document("2477", EDocumentType.CPF);
+            Assert.IsTrue(doc.Invalid);
+        }
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow("14212154008")]
+        [DataRow("00012154008")]
+        public void ShouldReturnSuccessWhenCPFIsValid(string cpf)
+        {
+            var doc = new Document(cpf, EDocumentType.CPF);
+            Assert.IsTrue(doc.Valid);
+        }
+    }
+}
