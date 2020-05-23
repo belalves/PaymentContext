23e65870baf84e9c62b68ebdba2f0760684645b2
Isabela Alves <bel.alves2012@gmail.com>
2020-05-23 12:48:09 -0300

    Criando um teste para a classe CreateCredCardSubscriptionCommand.

---
+++ PaymentContext/PaymentContext.Tests/Commands/CreateCredCardSubscriptionCommandTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentContext.Tests.Commands
+{
+    [TestClass]
+    public class CreateCredCardSubscriptionCommandTests
+    {
+        [TestMethod]
+        public void ShouldReturnErrorNameIsInvalid()
+        {
+            var command = new CreateCredCardSubscriptionCommand();
+            command.FirstName = "";
+
+            command.Validate();
+            Assert.AreEqual(false, command.Valid);
+        }
+
+    }
+}
