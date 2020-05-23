91fb8c65eff7e4397603b4b64bcd639255d3f279
Isabela Alves <bel.alves2012@gmail.com>
2020-05-23 12:42:39 -0300

    Criamos essa classe abstrata "ValueObject" no projeto Shared, para ficar mais visiveis, se eu precisar de algo comum ai incluo aqui.

--- PaymentContext/PaymentContext.Shared/ValueObjects/ValueObject.cs
+++ PaymentContext/PaymentContext.Shared/ValueObjects/ValueObject.cs
@@ -5,10 +5,7 @@ using System.Text;
 
 namespace PaymentContext.Shared.ValueObjects
 {
-    public class ValueObject : Notifiable
+    public abstract class ValueObject : Notifiable
     {
-        //criamos essa classe para ficar mais visiveis
-        //se eu precisar de algo comum ai incluo aqui
-
     }
 }
